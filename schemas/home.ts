85c7be57aebcb977a3022a56e9d766b14624088e
Gideon Assafuah <gassafuah@gmail.com>
2023-10-15 14:51:44 -0400

    update home cass

--- schemas/home.ts
+++ schemas/home.ts
@@ -4,27 +4,82 @@ export default {
   title: 'Home',
   fields: [
     {
+      name: 'myId',
+      type: 'number',
+      title: 'Position',
+    },
+    {
       name: 'title',
       type: 'string',
-      title: 'title',
+      title: 'Title',
+    },
+    {
+      name: 'subtitle',
+      type: 'string',
+      title: 'Subtitle',
     },
     {
       name: 'body',
       type: 'array',
-      title: 'body',
+      title: 'Body',
       of: [
         {
-          type: 'block'
-        }
-      ]
+          type: 'block',
+        },
+      ],
     },
     {
       name: 'image',
       type: 'image',
-      title: 'image',
+      title: 'Image',
       options: {
-        hotspot: true
-      }
+        hotspot: true,
+      },
+    },
+    {
+      name: 'component',
+      type: 'string',
+      title: 'Component',
+    },
+    {
+      name: 'description',
+      type: 'string',
+      title: 'Description',
+    },
+    {
+      name: 'backimage',
+      type: 'image',
+      title: 'Background Image',
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: 'externalLink',
+      type: 'string',
+      title: 'External Link',
+    },
+    {
+      name: 'list',
+      type: 'array',
+      title: 'Resource List',
+      of: [
+        {
+          type: 'object',
+          fields: [
+            {
+              name: 'name',
+              title: 'Name',
+              type: 'string',
+            },
+            {
+              name: 'url',
+              title: 'URL',
+              type: 'string',
+            },
+          ],
+        },
+      ],
     },
   ],
 }
